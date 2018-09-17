7bf1bb9b0da1e912ad83c97f9f8ac97097fe1382
heogong <heogong@gmail.com>
2018-09-17 21:14:18 +0900

    진행중

--- WeatherProject.js
+++ WeatherProject.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 
-import OpenWeatherMap from "./open_weather_map";
 import Forecast from "./Forecast";
+import OpenWeatherMap from "./open_weather_map";
 
 export default class WeatherProject extends Component {
   constructor(props) {
@@ -21,46 +21,66 @@ export default class WeatherProject extends Component {
     let content = null;
     if (this.state.forecast !== null) {
         content = (
-            <Forecast
-                main={this.state.forecast.main}
-                description={this.state.forecast.description}
-                temp={this.state.forecast.temp}
-            />
+          <Forecast
+              main={this.state.forecast.main}
+              description={this.state.forecast.description}
+              temp={this.state.forecast.temp}
+          />
         );
     }
     return (
-        <View style={styles.container}>
-            <Text style={styles.welcome}>You input {this.state.zip}</Text>
-            {content}
-            <TextInput
-                style={styles.input}
-                onSubmitEditing={this._handleTextChange}
-            />
-        </View>
+      <View style={styles.container}>
+        <Image
+          source={require('./batman.jpg')}
+          resizeMode='cover'
+          style={styles.backdrop}
+        >
+          <View style={styles.overlay}>
+            <View style={styles.row}>
+              <Text style={styles.mainText}>
+                Current weather for
+              </Text>
+              <View Style={styles.zipContainer}>
+                <TextInput
+                  style={[styles.zipCode, styles.mainText]}
+                  onSubmitEditing={event => this._handleTextChange(event)}
+                />
+              </View>
+            </View>
+              {content}
+          </View>
+        </Image>
+      </View>
     );
   }
 }
+const baseFontSize = 16;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+  container: {flex: 1, alignItems: 'center', paddingTop: 30},
+  backdrop: {flex: 1, flexDirection: 'column'},
+  overlay: {
+    paddingTop: 5,
+    backgroundColor: '#000000',
+    opacity: 0.5,
+    flexDirection: 'column',
+    alignItems: 'center'
   },
-  welcome : {
-    fontSize : 20,
-    textAlign : 'center',
-    margin: 10
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'flex-start',
+    padding: 30
   },
-  input : {
-    fontSize : 20,
-    borderWidth : 2,
-    padding : 2,
-    height: 40,
-    width : 100,
-    textAlign: 'center'
-  }
+  zipContainer: {
+    height: baseFontSize + 10,
+    borderBottomColor : '#DDDDDD',
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginTop: 3
+  },
+  zipCode: {flex:1, flexBasis: 1, width: 50, height: baseFontSize},
+  mainText: {fontSize: baseFontSize, color: '#FFFFFF'}
 });
 
 //export default WeatherProject;
--- open_weather_map.js
+++ open_weather_map.js
@@ -1,14 +1,18 @@
 const WEATHER_API_KEY = 'f2355b0a945709e7142a53c6ad6c27f8';
-const API_STEM = 'https://api.openweathermap.org/data/2.5/forecast?';
+//const API_STEM = 'https://api.openweathermap.org/data/2.5/forecast?';
+const API_STEM = 'https://api.openweathermap.org/data/2.5/weather?';
 
 function zipUrl(zip) {
-    return '${API_STEM}id=${zip}&APPID=${WEATHER_API_KEY}';
+    //return `${API_STEM}id=${zip}&APPID=${WEATHER_API_KEY}`;
+    return `${API_STEM}q=${zip}&units=imperial&APPID=${WEATHER_API_KEY}`;
 }
 
 function fetchForecast(zip) {
+    zip = 10001;
     return fetch(zipUrl(zip))
-        .then(response => respons.json())
+        .then(response => response.json())
         .then(responseJSON => {
+            console.log(responseJSON.weather[0])
             return {
                 main: responseJSON.weather[0].main,
                 description: responseJSON.weather[0].description,
@@ -16,7 +20,7 @@ function fetchForecast(zip) {
             };
         })
         .catch(error => {
-            console.error(error);
+            console.error('error : ', error);
         })
 }
 
