4f53fad1a14fb111de97618db19ad850514f402d
heogong <heogong@gmail.com>
2018-09-18 20:17:38 +0900

    버튼

--- App.js
+++ App.js
@@ -1,2 +1,4 @@
-import WeatherProject from "./WeatherProject";
-export default WeatherProject;
+//import WeatherProject from "./WeatherProject";
+//export default WeatherProject;
+import Button from "./Button";
+export default Button;
